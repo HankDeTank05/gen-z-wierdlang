7c77797153d7c6052206e9227b6b0b61f2053c7b
HankDeTank05 <henryhankholman@gmail.com>
2025-12-07 20:54:19 -0600

    Update genz.py

--- genz.py
+++ genz.py
@@ -6,10 +6,7 @@ var_func_name_pattern = r'[a-zA-Z_]{1}\w*'
 indexing_pattern = r'\['+var_func_name_pattern+r'\]'
 str_lit_pattern = r'[\'"]{1}.*[\'"]{1}'
 comment_sl_pattern = r'(lowkey )'
-comment_ml_pattern = r'(highkey)((?:\s{0,1})(?:.*\n)*?(?:\s{0,1}))(on god)'
-'''
-(highkey\s?)(\s?on god)
-'''
+comment_ml_pattern = r'highkey([\s\S]*?)on god'
 
 def transpile(code: str) -> str:
 	'''converts genz code into python code'''
@@ -18,59 +15,57 @@ def transpile(code: str) -> str:
 		# commenting
 		
 		# lowkey --> #
-		(r'(lowkey)', r'#'),
+		(r'lowkey', r'#'),
 		# highkey _ on god --> """ _ """
-		# (comment_ml_pattern, r'"""\2"""'),
-		(r'(highkey)', '"""'),
-		(r'(on god)', '"""'),
+		(comment_ml_pattern, r'"""\1"""'),
 
 		# other basic stuff
 
 		# spill ? tea --> print(?)
-		(r'(spill )(.*)( tea)', r'print(\2)'),
+		(r'spill (.*) tea', r'print(\1)'),
 		# sip ? tea --> input(?)
-		(r'(sip )(.*)( tea)', r'input(\2)'),
+		(r'sip (.*) tea', r'input(\1)'),
 		# is giving --> =
-		(r'( is giving )', ' = '),
+		(r' is giving ', ' = '),
 		# <val> is <type>-core --> <type>(<val>)
-		(r'(\w*)( is )(\w*)(-core)', r'\3(\1)'),
+		(r'(\w*) is (\w*)-core', r'\2(\1)'),
 
 		# literal values
 
 		# mid --> None
-		(r'(mid)', 'None'),
+		(r'mid', 'None'),
 		# green flag --> True
-		(r'(green flag)', 'True'),
+		(r'green flag', 'True'),
 		# red flag --> False
-		(r'(red flag)', 'False'),
+		(r'red flag', 'False'),
 
 		# if stuff
 
 		# POV --> if
-		(r'(POV)', 'if'),
+		(r'POV', 'if'),
 		# we're so back --> elif
-		(r"(we're so back)", 'elif'),
+		(r"we're so back", 'elif'),
 		# it's so over --> else
-		(r"(it's so over)", 'else'),
+		(r"it's so over", 'else'),
 		# bet --> :
-		(r'( bet)', ':'),
+		(r' bet', ':'),
 
 		# assertions
 
 		# be fucking for real ? --> assert(?)
-		(r'(be fucking for real )(.*)', r'assert(\2)'),
+		(r'be fucking for real (.*)', r'assert(\1)'),
 
 		# loop stuff
 
 		# cooking with --> while
-		(r'(cooking with)', 'while'),
+		(r'cooking with', 'while'),
 		# say less --> continue
-		(r'(say less)', 'continue'),
+		(r'say less', 'continue'),
 		# crash out --> break
-		(r'(crash out)', 'break'),
+		(r'crash out', 'break'),
 
 		# bestie _ aura farming _ --> for _ in _
-		(r'(bestie )(.*?)( aura farming )(.*)', r'for \2 in \4'),
+		(r'bestie (.*?) aura farming (.*)', r'for \1 in \2'),
 
 		# comparison ops
 
@@ -79,33 +74,33 @@ def transpile(code: str) -> str:
 		# A vibe checks B --> A != B
 		(r'( vibe checks )', ' != '),
 		# A ate B and left no crumbs --> A > B
-		(r'(.*)( ate )(.*)( and left no crumbs)', r'\1 > \3'),
+		(r'(.*) ate (.*) and left no crumbs', r'\1 > \2'),
 		# A ate B --> A >= B
-		(r'(.*)( ate )(.*)', r'\1 >= \3'),
+		(r'(.*) ate (.*)', r'\1 >= \2'),
 		# A got cancelled and ratioed by B --> A < B
-		(r'(.*)( got cancelled and ratioed by )(.*)', r'\1 < \3'),
+		(r'(.*) got cancelled and ratioed by (.*)', r'\1 < \2'),
 		# A got cancelled by B --> A <= B
-		(r'(.*)( got cancelled by )(.*)', r'\1 <= \3'),
+		(r'(.*) got cancelled by (.*)', r'\1 <= \2'),
 		# _ in _
-		(r'( aura farming )', ' in '),
+		(r' aura farming ', ' in '),
 
 		# list stuff
 
 		# gyatt --> []
-		(r'(gyatt)', '[]'),
+		(r'gyatt', '[]'),
 		# pulled --> .append
-		(r'( pulled )(.*)', r'.append(\2)'),
+		(r' pulled (.*)', r'.append(\1)'),
 		# pull up _ --> [_]
-		(r'( pull up )([\d+\w+])', r'[\2]'),
+		(r' pull up ([\d+\w+])', r'[\1]'),
 		# fit check <list> --> len(<list>)
-		(r'(fit check )((?:'+var_func_name_pattern+r')(?:'+indexing_pattern+r')*)', r'len(\2)'),
+		(r'fit check ((?:'+var_func_name_pattern+r')(?:'+indexing_pattern+r')*)', r'len(\1)'),
 
 		# function stuff
 
 		# finna --> def
-		(r'([\t ]*)(finna)', r'\1def'),
+		(r'finna', 'def'),
 		# clapback with --> return
-		(r'(clapback with )', 'return '),
+		(r'clapback with ', 'return '),
 	]
 
 	python_code = code
